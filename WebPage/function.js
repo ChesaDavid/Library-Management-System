2e04ea601755b3cca5344460b713d90a967ad21a
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-01-03 18:30:25 +0200

    Working on the web varient

---
+++ WebPage/function.js
@@ -0,0 +1,9 @@
+function AddBookButton(){
+    document.getElementById("ControlPanel2").style.display = none;
+    document.getElementById("FirstButton").style.display = block;
+    document.getElementById("FirstButton-BookName").style.display = block;
+    document.getElementById("AuthorName").style.display = block;
+}
+function SearchButton(){
+
+}
